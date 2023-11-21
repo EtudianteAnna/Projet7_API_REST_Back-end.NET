3299c0f22934c87113ee702ae5ce1df958516a5c
EtudianteAnna <123629810+EtudianteAnna@users.noreply.github.com>
2023-11-21 18:37:32 +0100

    p7/MAJ de la classe Program: ajout Token JWT et les services

---
+++ P7CreateRestApi/Data/SeedData.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+using P7CreateRestApi.Data;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new LocalDbContext(
+            serviceProvider.GetRequiredService<DbContextOptions<LocalDbContext>>()))
+        {
+            SeedRoles(context);
+            SeedUsers(context);
+
+             // Ajoutez d'autres méthodes de séquence si nécessaire
+        }
+    }
+
+    private static void SeedRoles(LocalDbContext context)
+    {
+        // Code de séquence pour les rôles
+        // ...
+    }
+
+    private static void SeedUsers(LocalDbContext context)
+    {
+        // Code de séquence pour les utilisateurs
+        // ...
+    }
+}
--- P7CreateRestApi/Program.cs
+++ P7CreateRestApi/Program.cs
@@ -1,75 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using P7CreateRestApi.Data;
-using P7CreateRestApi.Repositories;
-
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        ConfigureServices(builder.Services);
-
-        var app = builder.Build();
-
-        Configure(app, builder.Environment);
-
-        app.Run();
-    }
-
-    private static void ConfigureServices(IServiceCollection services)
-    {
-        services.AddDbContext<LocalDbContext>(options =>
-            options.UseSqlServer("YourConnectionString"));
-
-        services.AddScoped<IBidListRepository, BidListRepository>();
-        services.AddScoped<ICurvePointRepository, CurvePointRepository>();
-        services.AddScoped<IRatingRepository, RatingRepository>();
-        _ = services.AddScoped<IRuleNameRepository, RuleNameRepository>();
-        services.AddScoped<ITradeRepository, TradeRepository>();
-        services.AddScoped<IUserRepository, UserRepository>();
-
-        services.AddCors(options =>
-        {
-            options.AddPolicy("MyPolicy", builder =>
-            {
-                _ = builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-            });
-        });
-
-        services.AddControllers();
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
-        });
-    }
-
-    private static void Configure(WebApplication app, IHostEnvironment env)
-    {
-        if (env.EnvironmentName == "Development")
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API v1");
-            });
-        }
-
-        app.UseHttpsRedirection();
-        app.UseCors("MyPolicy");
-        app.MapControllers();
-    }
-}
-
-/*using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using P7CreateRestApi.Data;
 using P7CreateRestApi.Repositories;
+using System.Text;
 
 internal class Program
 {
@@ -88,6 +23,9 @@ internal class Program
 
     private static void ConfigureServices(IServiceCollection services)
     {
+        // Ajoutez le service MVC pour les contrôleurs
+        services.AddMvc();
+        // Ajoutez les services DbContext et repositories
         services.AddDbContext<LocalDbContext>(options =>
             options.UseSqlServer("YourConnectionString"));
 
@@ -98,35 +36,57 @@ internal class Program
         services.AddScoped<ITradeRepository, TradeRepository>();
         services.AddScoped<IUserRepository, UserRepository>();
 
-        services.AddCors(options =>
+        // Configuration du service d'authentification JWT
+        services.AddSwaggerGen(swaggerGenOptions =>
         {
-            options.AddPolicy("MyPolicy", builder =>
+            swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo
             {
-                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                Title = "Titre",
+                Version = "1.0",
+                Contact = new OpenApiContact
+                {
+                    Email = "bidon"
+                }
             });
         });
 
-        services.AddControllers();
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
-        });
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "your-issuer", // Mettez votre émetteur (issuer) JWT ici
+                    ValidAudience = "your-audience", // Mettez votre audience JWT ici
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your-secret-key")) // Mettez votre clé secrète JWT ici
+                };
+            });
+
     }
 
     private static void Configure(WebApplication app, IHostEnvironment env)
     {
+        // ... autres configurations
+
         if (env.EnvironmentName == "Development")
         {
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API v1");
+                c.RoutePrefix = "";
             });
         }
 
         app.UseHttpsRedirection();
         app.UseCors("MyPolicy");
+        app.UseAuthentication(); // Ajoutez cette ligne pour activer l'authentification JWT
         app.MapControllers();
     }
-}*/
+}
+
+                   
